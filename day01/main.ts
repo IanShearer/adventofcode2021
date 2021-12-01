4b17c9963d0a60b98ddd825309e1b22c5ccb7297
IanShearer <iangcshearer@gmail.com>
2021-12-01 00:08:10 -0500

    day 1 complete

---
+++ day01/main.ts
@@ -0,0 +1,58 @@
+import * as colors from 'https://deno.land/std/fmt/colors.ts';
+
+type part =  'one' | 'two';
+
+function aoc(p: part, fn: (args: number[]) => number) {
+    return function (args: number[]) {
+        const start = performance.now();
+        const ans: number = fn(args);
+        const finish = performance.now();
+    
+        console.log(`Part ${p}: ${colors.green(ans.toString())}\nTime: ${finish - start}ms`)
+    }
+}
+
+function partOne(nums: number[]): number {
+
+    let counts: number = -1;
+    let previousNumber: number = 0;
+    for (const n of nums) {
+        if (previousNumber < n) {
+            counts++;
+        }
+        previousNumber = n;
+    }
+
+    return counts;
+}
+
+function partTwo(nums: number[]): number {
+
+    let counts: number = -1;
+    let previousNumber: number = 0;
+    for (let i = 0; i < nums.length - 2; i++) {
+        const n = nums[i] + nums[i+1] + nums[i+2];
+        if (previousNumber < n) {
+            counts++;
+        }
+        previousNumber = n;
+    }
+
+    return counts;
+}
+
+async function main() {
+    const startTime = performance.now();
+
+    const input = await Deno.readTextFile("./input.txt");
+    const nums: number[] = input.
+                            split("\n").map(n => parseInt(n));
+
+    aoc('one', partOne)(nums);
+    aoc('two', partTwo)(nums);
+
+    const finishTime = performance.now();
+    console.log(`Total time: ${finishTime - startTime}ms`)
+}
+
+main();
